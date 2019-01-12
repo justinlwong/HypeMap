a90f2abf41aad604bdea7ef9fb6ed0096919b1c0
justinlwong <justin.lcy.wong@gmail.com>
2019-01-12 17:38:20 -0500

    Requests permission to access fine location and opens up the map at the user's current location with a marker

--- app/src/main/java/justin/apackage/com/hypemap/MapsActivity.kt
+++ app/src/main/java/justin/apackage/com/hypemap/MapsActivity.kt
@@ -1,43 +1,100 @@
 package justin.apackage.com.hypemap
 
-import android.support.v7.app.AppCompatActivity
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
-
+import android.support.v4.app.ActivityCompat
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
+        GoogleMap.OnMarkerClickListener {
+    override fun onMarkerClick(p0: Marker?) = false
 
     private lateinit var mMap: GoogleMap
+    private lateinit var mCurLocation : Location
+    private lateinit var mLocationClient : FusedLocationProviderClient
+
+    companion object {
+        private const val PERMISSION_LOCATION_REQUEST_CODE = 1
+        private const val TAG = "MapsActivity"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate called")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        mLocationClient = LocationServices.getFusedLocationProviderClient(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        mMap.getUiSettings().setZoomControlsEnabled(true)
+        mMap.setOnMarkerClickListener(this)
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        setUpMapFragment()
+    }
+
+    private fun setUpMapFragment() {
+        Log.d(TAG, "Setting up Map Fragment")
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.d(TAG, "Getting permissions")
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), PERMISSION_LOCATION_REQUEST_CODE
+            )
+            return
+        } else {
+            moveToCurrentLocation()
+        }
+    }
+
+    private fun addMarkerAtLocation(location: LatLng) {
+        val markerOptions = MarkerOptions().position(location)
+        mMap.addMarker(markerOptions)
+    }
+
+    private fun moveToCurrentLocation() {
+        Log.d(TAG, "Set up current location on map")
+        mMap.isMyLocationEnabled = true
+
+        mLocationClient.lastLocation.addOnSuccessListener(this) { location ->
+            if (location != null) {
+                mCurLocation = location
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                addMarkerAtLocation(currentLatLng)
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 18f))
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSION_LOCATION_REQUEST_CODE -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    moveToCurrentLocation()
+                }
+                return
+            }
+        }
     }
 }
